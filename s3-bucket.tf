3eff6b0a3abe433e5eb79eb6f2b80c37e022a13f
PavaniVemasani18 <vemasani.pavani@gmail.com>
2024-10-02 21:23:42 +0530

    add dynamodb

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket         = "bucketinfo23"
-    key            = "remote-state"
+    key            = "remote-state1"
     region         = "us-east-1"
     dynamodb_table = "lock-remotestate"
   }
